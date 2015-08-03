51d4b85d5e05009002d0ff931f5f775a76840bd9
François Chapuis <afnarel@gmail.com>
2015-08-03 15:15:56 +0200

    Fix import error

--- fluent_comments/models.py
+++ fluent_comments/models.py
@@ -6,7 +6,7 @@ from django_comments.managers import CommentManager
 if VERSION < (1, 8):
     from django.contrib.contenttypes.generic import GenericRelation
 else:
-    from django.contrib.contenttypes.generic.fields import GenericRelation
+    from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.sites.models import get_current_site
 from django.core.mail import send_mail
 from django.dispatch import receiver
